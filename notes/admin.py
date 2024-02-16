af1a406394a55a86f37c248584daa35821f81b65
spellsharp <svcodin22@gmail.com>
2024-02-16 15:46:29 +0530

    Something

--- notes/admin.py
+++ notes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Notes
 # Register your models here.
 class NotesAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'deadline' , 'created_at', 'updated_at')
+    fields = ('title', 'description', 'deadline', 'updated_at')
     list_display = ('title', 'description', 'deadline', 'created_at', 'updated_at')
 
 admin.site.register(Notes, NotesAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 import uuid
 import datetime
-# Create your models here.
+
 class Notes(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    deadline = models.DateTimeField(null=True, blank=True)
-    created_at = models.DateTimeField(null=True)
+    deadline = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, null=True, blank=True)
     updated_at = models.DateTimeField(null=True)
 
     def save(self, *args, **kwargs):
-        self.updated_at = datetime.date.today()
+        self.updated_at = datetime.datetime.now()
         super().save(*args, **kwargs)
 
     def __str__(self):
