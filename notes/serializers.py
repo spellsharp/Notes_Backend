a788ff82803ae4fb0239dda3289604830d0493f1
spellsharp <svcodin22@gmail.com>
2024-02-19 05:53:21 +0530

    <feat>:[Added a nested serializer to NoteSerializer]

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Notes
+from django.contrib.auth.models import User
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 class NoteSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer() 
+
     class Meta:
         model = Notes
         fields = '__all__'
